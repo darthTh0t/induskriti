9a6387ad82f7aaa9f4e5b60f706c141a9b85a371
arnav-sahoo <arnav.sahoo.97@gmail.com>
2023-09-02 17:33:53 +0530

    added file upload form in admin_gallery

--- app/admin.py
+++ app/admin.py
@@ -3,6 +3,7 @@
 from flask import Blueprint, render_template
 from .models import Customer
 from .forms import GalleryForm
+from app import db
 
 admin_page = Blueprint('admin', __name__, template_folder='templates/admin', static_folder='static/admin')
 
@@ -20,4 +21,4 @@ def customer_list():
 @admin_page.route('/admin-gallery/')
 def gallery_list():
     form = GalleryForm()
-    return render_template('admin_gallery.html')
+    return render_template('admin_gallery.html', form=form)

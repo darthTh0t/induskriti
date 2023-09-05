b5dfd506139839eb830db419e600df3476ee37cf
Arnav Sahoo <arnav.sahoo.97@gmail.com>
2023-09-05 12:19:14 +0530

    added firebase SDK for testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import text, MetaData, Table
 from flask_login import LoginManager
+import firebase_admin
+from firebase_admin import credentials
 
+#get the firebase-credentials.json file
+credentials_path = os.path.join(os.path.dirname(__file__), '..', 'firebase-credentials.json')
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -13,6 +18,10 @@ login_manager.login_view = 'login'
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object("config.Config")
+
+    #Initialise firebase admin SDK
+    cred = credentials.Certificate(credentials_path)
+    firebase_admin.initialize_app(cred)
     
     db.init_app(app)
     migrate.init_app(app, db)
--- app/blueprints/shop_page.py
+++ app/blueprints/shop_page.py
@@ -1,9 +1,19 @@
 from flask import Blueprint, redirect, request, flash, render_template, url_for
 from app import db
+import firebase_admin
+from firebase_admin import storage
 
 
 shop_page = Blueprint('shop', __name__, template_folder='templates/shop', static_folder='static/shop')
 
+def get_image_urls():
+	bucket = storage.bucket(app=firebase_admin.get_app())
+	blobs = bucket.list_blobs(prefix="images/")
+	image_urls = [blob.public_url for blob in blobs]
+	return image_urls
+
+
 @shop_page.route('/')
 def shop_base():
-	return render_template('shop/shop_base.html')
+	image_urls = get_image_urls()
+	return render_template('shop/shop_base.html', image_urls=image_urls)

b8fc17b355c4a0ee180aa0f9abc18a67cd5b1263
Arnav Sahoo <arnav.nerd42@gmail.com>
2023-09-09 12:29:37 +0530

    removed .env file

--- app/blueprints/shop/__init__.py
+++ app/blueprints/shop/__init__.py
@@ -4,19 +4,27 @@ from firebase_admin import storage, credentials
 from flask import Blueprint, redirect, request, flash, render_template, url_for, current_app
 from app import db
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
 
 
 shop_page = Blueprint('shop', __name__, template_folder='templates/', static_folder='static/')
 
 def get_image_urls():
+    load_dotenv()
+    service_account_json = os.getenv("FIREBASE_ADMIN_CREDENTIALS_JSON")
+    
     # initializing firebase-admin SDK
     if not firebase_admin._apps:
-        cred = credentials.Certificate('induskritistorage-firebase-adminsdk-b2qf0-33fe67ff3b.json')
+        cred = credentials.Certificate(service_account_json)
         firebase_admin.initialize_app(cred)
+    
     # getting a reference to the Firebase Storage bucket
     bucket = storage.bucket(name="induskritistorage.appspot.com")
+    
     # listing the images in the "images" directory in your Firebase Storage bucket
     blobs = bucket.list_blobs(prefix="images/")
+    
     # creating a list of image URLs by directly using blob.generate_signed_url
     image_urls = [blob.generate_signed_url(expiration=timedelta(minutes=15), method="GET") \
                    for blob in blobs if not blob.name.endswith('/')]
@@ -25,4 +33,4 @@ def get_image_urls():
 @shop_page.route('/', methods=['GET'])
 def shop_base():
     image_urls = get_image_urls()
-    return render_template('shop/shop_base.html', image_urls=image_urls)
+    return render_template('shop_base.html', image_urls=image_urls)

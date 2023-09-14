343665455d8e4c2aaf02b6caa51222864b5ad2eb
Arnav Sahoo <arnav.sahoo.97@gmail.com>
2023-09-14 22:23:51 +0530

    sent firebase_image_urls to template

--- app/blueprints/gallery/__init__.py
+++ app/blueprints/gallery/__init__.py
@@ -2,9 +2,30 @@ from flask import Blueprint, flash ,redirect, render_template, request, url_for
 from app.blueprints.admin.models import ImageHash
 from app.blueprints.admin.firebase import File
 
+#calling the image url_list
+
 gallery_page = Blueprint('gallery', __name__, template_folder='templates/', static_folder='static/')
 
 @gallery_page.route('/')
 def index():
 	image_urls = File().image_download()
 	return render_template('gallery.html', image_urls=image_urls)
+
+
+#@gallery_page.route('/get_image')
+#def get_image():
+#	index = request.args.get("index")
+#	try:
+#		index = int(index)
+#		if 0 <= index < len(image_urls):
+#			image_url = image_urls[index]
+#			response = requests.get(image_url)
+#			if response.status_code == 200:
+#				headers = {'Content-Type': response.headers['Content-Type']}
+#				return Response(response.content, headers=headers)
+#			else:
+#				return 'Image Not Found', 404
+#		else:
+#			return 'Invalid Index', 400
+#	except ValueError:
+#		return 'Invalid Index', 400
